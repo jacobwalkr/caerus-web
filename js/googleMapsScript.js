59a68cfa50c6361d62e4845e129c4b5ade64a322
Barnaby Taylor <taylorbarnaby@gmail.com>
2013-08-07 17:52:29 +0100

    IT FINALLY WORKS. SCREW YOU JS PLUGIN.

--- js/googleMapsScript.js
+++ js/googleMapsScript.js
@@ -3,56 +3,49 @@ var map;
 var circle;
 function initialize() {
     var mapOptions = {
-        center: new google.maps.LatLng(54,-2),
+        center: new google.maps.LatLng(54, -2),
         zoom: 6,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
 }
-function addPin() {
-    var pin = new google.maps.Marker({
-        map: map,
-        position: new google.maps.LatLng(54,-2),
-        draggable: true,
-    });
-}
 google.maps.event.addDomListener(window, "load", initialize);
-var pinColour = "#e74c3c"
+var pinColour = "#e74c3c";
 //Add pin to map
 function codeAddress(pinColour) {
-    var customIcon
-    switch ($("#cd-dropdown").value) {
-        case 1:
-            if (pinColour == "#e74c3c") {
+    var customIcon;
+    switch ($("div.cd-dropdown span").first().text()) {
+        case "Clothing":
+            if (pinColour === "#e74c3c") {
                 customIcon = "img/redTeePin.svg";
             }
-            else if (pinColour == "#2980b9") {
+            else if (pinColour === "#2980b9") {
                 customIcon = "img/blueTeePin.svg";
-            };
+            }
         break;
-        case 2:
-            if (pinColour == "#e74c3c") {
+        case "Computers":
+            if (pinColour === "#e74c3c") {
                 customIcon = "img/redComputerPin.svg";
             }
-            else if (pinColour == "#2980b9") {
+            else if (pinColour === "#2980b9") {
                 customIcon = "img/blueComputerPin.svg";
-            };
+            }
         break;
-        case 4:
-            if (pinColour == "#e74c3c") {
+        case "Mobile Devices":
+            if (pinColour === "#e74c3c") {
                 customIcon = "img/redPhonePin.svg";
             }
-            else if (pinColour == "#2980b9") {
+            else if (pinColour === "#2980b9") {
                 customIcon = "img/bluePhonePin.svg";
-            };
+            }
         break;
-        case -1:
-            if (pinColour == "#e74c3c") {
+        default:
+            if (pinColour === "#e74c3c") {
                 customIcon = "img/redPin.svg";
             }
-            else if (pinColour == "#2980b9") {
+            else if (pinColour === "#2980b9") {
                 customIcon = "img/bluePin.svg";
-            };
+            }
         break;
     }
     var address = document.getElementById("address").value;
@@ -63,7 +56,7 @@ function codeAddress(pinColour) {
             var pin = {
                 url:customIcon,
                 scaledSize: new google.maps.Size(20,34.09,"px","px")
-            }
+            };
             window.marker = new google.maps.Marker({
                 map: map,
                 position: results[0].geometry.location,
@@ -80,11 +73,11 @@ function codeAddress(pinColour) {
             });
             circle.bindTo("center",marker,"position");
         }
-        else if (status = "ZERO_RESULTS") {
+        else if (status === "ZERO_RESULTS") {
             var pin = {
                 url:customIcon,
                 scaledSize: new google.maps.Size(20,34.09,"px","px")
-            }
+            };
             window.marker = new google.maps.Marker({
                 map: map,
                 position: map.getCenter(),
