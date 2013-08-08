9a5c3eeb16ad73eba427c1f466b5aefb4056654d
Barnaby Taylor <taylorbarnaby@gmail.com>
2013-08-08 15:52:36 +0100

    Lightboxes Function

--- js/googleMapsScript.js
+++ js/googleMapsScript.js
@@ -1,8 +1,6 @@
-var geocoder;
-var map;
-var circle;
-var lostLightboxHTML = '<h1 class="lostHeader">Edit pin information</h1><hr class="lostRule"><form><table><tr><td><label class="lostLabel">What have you lost?</label></td><td><input type="text" id= "title" class="markerLostForm" placeholder="Item"></td></tr><tr><td><label class="lostLabel">Enter an address or postcode for the pin (optional):</label></td><td><input type="text" placeholder="Address" id="address" class="markerLostForm"></td></tr><tr><td><label class="lostLabel">Not sure where you lost it? Set a larger area:</label></td><td><input type="text" class="markerLostForm" id="circleRadiusInput" placeholder="0"></td></tr><tr><td></td><td><input type="button" id="submitLostMarker" onclick="submitLostMarker()" value="Add marker"></td></tr></table></form>';
-var foundLightboxHTML = '<h1 class="foundHeader">Edit pin information</h1><hr class="foundRule"><form><table><tr><td><label class="foundLabel">What have you found?</label></td><td><input type="text" id= "title" class="markerFoundForm" placeholder="Item"></td></tr><tr><td><label class="foundLabel">Enter an address or postcode for the pin (optional):</label></td><td><input type="text" placeholder="Address" id="address" class="markerFoundForm"></td></tr><tr><td><label class="foundLabel">Not sure where you found it? Set a larger area:</label></td><td><input type="text" class="markerFoundForm" id="circleRadiusInput" placeholder="0"></td></tr><tr><td></td><td><input type="button" id="submitFoundMarker" onclick="submitFoundMarker()" value="Add marker"></td></tr></table></form>';
+var geocoder, map, circle;
+var lostLightboxHTML = '<h1 class="lostHeader">Edit pin information</h1><hr class="lostRule"><form><table><tr><td><label class="lostLabel">What have you lost?</label></td><td><input type="text" id= "title" class="markerLostForm" placeholder="Item"></td></tr><tr><td><label class="lostLabel">Enter an address or postcode for the pin (optional):</label></td><td><input type="text" placeholder="Address" id="address" class="markerLostForm"></td></tr><tr><td><label class="lostLabel">Not sure where you lost it? Set an area in meters:</label></td><td><input type="text" class="markerLostForm" id="circleRadiusInput" placeholder="0"></td></tr><tr><td></td><td><input type="button" id="submitLostMarker" onclick="codeAddress(\'#e74c3c\')" value="Add marker"></td></tr></table></form>';
+var foundLightboxHTML = '<h1 class="foundHeader">Edit pin information</h1><hr class="foundRule"><form><table><tr><td><label class="foundLabel">What have you found?</label></td><td><input type="text" id= "title" class="markerFoundForm" placeholder="Item"></td></tr><tr><td><label class="foundLabel">Enter an address or postcode for the pin (optional):</label></td><td><input type="text" placeholder="Address" id="address" class="markerFoundForm"></td></tr><tr><td><label class="foundLabel">Not sure where you found it? Set an area in meters:</label></td><td><input type="text" class="markerFoundForm" id="circleRadiusInput" placeholder="0"></td></tr><tr><td></td><td><input type="button" id="submitFoundMarker" onclick="codeAddress(\'#2980b9\')" value="Add marker"></td></tr></table></form>';
 function initialize() {
     var mapOptions = {
         center: new google.maps.LatLng(54, -2),
@@ -13,6 +11,48 @@ function initialize() {
 }
 google.maps.event.addDomListener(window, "load", initialize);
 var pinColour = "#e74c3c";
+//Function added to JQuery to center an element
+jQuery.fn.center = function () {
+    this.css("position","absolute");
+    this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) + 
+                                                $(window).scrollTop()) + "px");
+    this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) + 
+                                                $(window).scrollLeft()) + "px");
+    return this;
+}
+//Draw a lightbox. Argument is lightbox id
+function drawLightbox(lightboxID) {
+    $("<div/>", {
+        id: lightboxID + "Greyout",
+        class: "lightboxGreyout"
+    }).prependTo("body");
+    $("<div/>", {
+        id: lightboxID,
+        class: "lightbox"
+    }).prependTo("body");
+    $("#" + lightboxID).center();
+}
+//Add marker lightbox content
+function addMarkerLightboxContent(pinHex) {
+    if (pinHex === "#e74c3c") {
+        $(lostLightboxHTML).appendTo("#markerLightbox");
+    }
+    else if (pinHex === "#2980b9") {
+        $(foundLightboxHTML).appendTo("#markerLightbox");
+    }
+}
+//Had to define this as one function to work with the html
+function lightboxAndContent(lightboxID,pinColour) {
+    drawLightbox(lightboxID);
+    addMarkerLightboxContent(pinColour);
+}
+//Remove lightbox and submit content
+function removeLightbox(lightboxID) {
+//    $("#title").value();
+    $("#" + lightboxID).remove();
+    var greyoutID = lightboxID + "Greyout";
+    $("#" + greyoutID).remove();
+}
 //Add pin to map
 function codeAddress(pinColour) {
     var customIcon;
@@ -79,8 +119,6 @@ function codeAddress(pinColour) {
         draggable: true,
         icon: pin
     });
-    drawLightbox("markerLightbox");
-    addMarkerLightboxContent(pinColour);
     google.maps.event.addListener(marker,"click",function() {drawLightbox("markerLightbox")});
     circle = new google.maps.Circle({
         map: map,
@@ -91,22 +129,21 @@ function codeAddress(pinColour) {
         radius: parseInt(document.getElementById("circleRadiusInput").value)
     });
     circle.bindTo("center",marker,"position");
-/*    var address = document.getElementById("address").value;
+    var address = document.getElementById("address").value;
     var geocoder = new google.maps.Geocoder();
+    var pin = {
+        url:customIcon,
+        scaledSize: new google.maps.Size(20,34.09,"px","px")
+    };
     geocoder.geocode( { "address": address}, function(results, status) {
         if (status == google.maps.GeocoderStatus.OK) {
             map.setCenter(results[0].geometry.location);
-            var pin = {
-                url:customIcon,
-                scaledSize: new google.maps.Size(20,34.09,"px","px")
-            };
             window.marker = new google.maps.Marker({
                 map: map,
                 position: results[0].geometry.location,
                 draggable: true,
                 icon: pin
             });
-            google.maps.event.addListener(marker,"click",drawLightbox(markerLightbox));
             circle = new google.maps.Circle({
                 map: map,
                 fillColor:pinColour,
@@ -118,30 +155,25 @@ function codeAddress(pinColour) {
             circle.bindTo("center",marker,"position");
         }
         else if (status === "ZERO_RESULTS") {
-            
+            window.marker = new google.maps.Marker({
+                map: map,
+                position: map.getCenter(),
+                draggable: true,
+                icon: pin
+            });
+            circle = new google.maps.Circle({
+                map: map,
+                fillColor: pinColour,
+                fillOpacity: 0.5,
+                strokeColor: pinColour,
+                center: window.marker.getPosition(),
+                radius: parseInt(document.getElementById("circleRadiusInput").value)
+            });
+            circle.bindTo("center",marker,"position");
         }
         else {
         alert("Geocode was not successful for the following reason: " + status);
         }
-    });*/
+    });
+    removeLightbox("markerLightbox");
 }
-//Draw a lightbox. Argument is lightbox id
-function drawLightbox(lightboxID) {
-    $("<div/>", {
-        id: lightboxID + "Greyout",
-        class: "lightboxGreyout"
-    }).prependTo("body");
-    $("<div/>", {
-        id: lightboxID,
-        class: "lightbox"
-    }).prependTo("body");
-};
-//Add marker lightbox content
-function addMarkerLightboxContent(pinHex) {
-    if (pinHex === "#e74c3c") {
-        $(lostLightboxHTML).appendTo("#markerLightbox");
-    }
-    else if (pinHex === "#2980b9") {
-        $(foundLightboxHTML).appendTo("#markerLightbox");
-    }
-};
