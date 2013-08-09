7a3c00daa23d03ca36fcf42247d87b1ff532337e
Barnaby Taylor <taylorbarnaby@gmail.com>
2013-08-09 02:11:08 +0100

    Tidied up the script a little

--- js/frontpageScript.js
+++ js/frontpageScript.js
@@ -1,7 +1,11 @@
 var geocoder, map, circle;
-var lostLightboxHTML = '<h1 class="lostHeader">Edit pin information</h1><hr class="lostRule"><form><table><tr><td><label class="lostLabel">What have you lost?</label></td><td><input type="text" id= "title" class="markerLostForm" placeholder="Item"></td></tr><tr><td><label class="lostLabel">Enter an address or postcode for the pin (optional):</label></td><td><input type="text" placeholder="Address" id="address" class="markerLostForm"></td></tr><tr><td><label class="lostLabel">Not sure where you lost it? Set an area in meters:</label></td><td><input type="text" class="markerLostForm" id="circleRadiusInput" placeholder="0"></td></tr><tr><td></td><td><input type="button" id="submitLostMarker" onclick="codeAddress(\'#e74c3c\')" value="Add marker"></td></tr></table></form>';
-var foundLightboxHTML = '<h1 class="foundHeader">Edit pin information</h1><hr class="foundRule"><form><table><tr><td><label class="foundLabel">What have you found?</label></td><td><input type="text" id= "title" class="markerFoundForm" placeholder="Item"></td></tr><tr><td><label class="foundLabel">Enter an address or postcode for the pin (optional):</label></td><td><input type="text" placeholder="Address" id="address" class="markerFoundForm"></td></tr><tr><td><label class="foundLabel">Not sure where you found it? Set an area in meters:</label></td><td><input type="text" class="markerFoundForm" id="circleRadiusInput" placeholder="0"></td></tr><tr><td></td><td><input type="button" id="submitFoundMarker" onclick="codeAddress(\'#2980b9\')" value="Add marker"></td></tr></table></form>';
+//html to be added to lightboxes
+var lostLightboxHTML = '<h1 class="lostHeader">Edit pin information</h1><hr class="lostRule"><form><table><tr><td><label class="lostLabel">What have you lost?</label></td><td><input type="text" id= "title" class="markerLostForm" placeholder="Item"></td></tr><tr><td><label class="lostLabel">Enter an address or postcode for the pin (optional):</label></td><td><input type="text" placeholder="Address" id="address" class="markerLostForm"></td></tr><tr><td><label class="lostLabel">Not sure where you lost it? Set an area in meters:</label></td><td><input type="text" class="markerLostForm" id="circleRadiusInput" placeholder="0"></td></tr><tr><td></td><td><input type="button" id="submitLostMarker" onclick="addPin(\'#e74c3c\')" value="Add marker"></td></tr></table></form>';
+var foundLightboxHTML = '<h1 class="foundHeader">Edit pin information</h1><hr class="foundRule"><form><table><tr><td><label class="foundLabel">What have you found?</label></td><td><input type="text" id= "title" class="markerFoundForm" placeholder="Item"></td></tr><tr><td><label class="foundLabel">Enter an address or postcode for the pin (optional):</label></td><td><input type="text" placeholder="Address" id="address" class="markerFoundForm"></td></tr><tr><td><label class="foundLabel">Not sure where you found it? Set an area in meters:</label></td><td><input type="text" class="markerFoundForm" id="circleRadiusInput" placeholder="0"></td></tr><tr><td></td><td><input type="button" id="submitFoundMarker" onclick="addPin(\'#2980b9\')" value="Add marker"></td></tr></table></form>';
 var loginLightboxHTML = '<iframe scr="loginForm.html"></iframe>';
+//Objects for comparison when creating markers
+redCategories = {"Clothing":"img/redTeePin.svg","Computers":"img/redComputerPin.svg","Keys":"img/redKeyPin.svg","Mobile Devices":"img/redPhonePin.svg","Wallets and Purses":"img/redPoundPin.svg","Other":"img/redPin.svg","Filter":"img/redPin.svg"}
+blueCategories = {"Clothing":"img/blueTeePin.svg","Computers":"img/blueComputerPin.svg","Keys":"img/blueKeyPin.svg","Mobile Devices":"img/bluePhonePin.svg","Wallets and Purses":"img/bluePoundPin.svg","Other":"img/bluePin.svg","Filter":"img/bluePin.svg"}
 function initialize() {
     var mapOptions = {
         center: new google.maps.LatLng(54, -2),
@@ -21,7 +25,6 @@ jQuery.fn.center = function () {
                                                 $(window).scrollLeft()) + "px");
     return this;
 }
-//Draw a lightbox. Argument is lightbox id
 function drawLightbox(lightboxID) {
     $("<div/>", {
         id: lightboxID + "Greyout",
@@ -33,7 +36,6 @@ function drawLightbox(lightboxID) {
     }).prependTo("body");
     $("#" + lightboxID).center();
 }
-//Add marker lightbox content
 function addMarkerLightboxContent(pinHex) {
     if (pinHex === "#e74c3c") {
         $(lostLightboxHTML).appendTo("#markerLightbox");
@@ -46,81 +48,30 @@ function lightboxAndContent(lightboxID,pinColour) {
     drawLightbox(lightboxID);
     addMarkerLightboxContent(pinColour);
 }
-//Remove lightbox and submit content
 function removeLightbox(lightboxID) {
 //    $("#title").value();
     $("#" + lightboxID).remove();
     var greyoutID = lightboxID + "Greyout";
     $("#" + greyoutID).remove();
 }
-//Add pin to map
-function codeAddress(pinColour) {
+function addPin(pinColour) {
     var customIcon;
-    var circleRadius = parseInt(document.getElementById("circleRadiusInput").value);
-    switch ($("div.cd-dropdown span").first().text()) {
-        case "Clothing":
-            if (pinColour === "#e74c3c") {
-                customIcon = "img/redTeePin.svg";
-            }
-            else if (pinColour === "#2980b9") {
-                customIcon = "img/blueTeePin.svg";
-            }
-        break;
-        case "Computers":
-            if (pinColour === "#e74c3c") {
-                customIcon = "img/redComputerPin.svg";
-            }
-            else if (pinColour === "#2980b9") {
-                customIcon = "img/blueComputerPin.svg";
-            }
-        break;
-        case "Keys":
-            if (pinColour === "#e74c3c") {
-                customIcon = "img/redKeyPin.svg";
-            }
-            else if (pinColour === "#2980b9") {
-                customIcon = "img/blueKeyPin.svg";
-            }
-        break;
-        case "Mobile Devices":
-            if (pinColour === "#e74c3c") {
-                customIcon = "img/redPhonePin.svg";
-            }
-            else if (pinColour === "#2980b9") {
-                customIcon = "img/bluePhonePin.svg";
-            }
-        break;
-        case "Wallets and Purses":
-            if (pinColour === "#e74c3c") {
-                customIcon = "img/redPoundPin.svg";
-            }
-            else if (pinColour === "#2980b9") {
-                customIcon = "img/bluePoundPin.svg";
-            }
-        break;
-        case "Filter":
-            alert("Please choose a category!");
-        break;
-        default:
-            if (pinColour === "#e74c3c") {
-                customIcon = "img/redPin.svg";
-            }
-            else if (pinColour === "#2980b9") {
-                customIcon = "img/bluePin.svg";
-            }
-        break;
+    var selectedCategory = $("div.cd-dropdown span").first().text();
+    var circleRadius = parseInt($("#circleRadiusInput").val());
+    if (pinColour === "#e74c3c") {
+        customIcon = redCategories[selectedCategory];
+    }
+    else if (pinColour === "#2980b9") {
+        customIcon = blueCategories[selectedCategory];
     }
     var pin = {
         url:customIcon,
         scaledSize: new google.maps.Size(20,34.09,"px","px")
     };
+    //Changes the address or postcode given to a Latlng
     var address = document.getElementById("address").value;
     var geocoder = new google.maps.Geocoder();
-    var pin = {
-        url:customIcon,
-        scaledSize: new google.maps.Size(20,34.09,"px","px")
-    };
-    geocoder.geocode( { "address": address}, function(results, status) {
+    geocoder.geocode( {"address": address}, function(results, status) {
         if (status == google.maps.GeocoderStatus.OK) {
             map.setCenter(results[0].geometry.location);
             window.marker = new google.maps.Marker({
@@ -131,9 +82,9 @@ function codeAddress(pinColour) {
             });
             circle = new google.maps.Circle({
                 map: map,
-                fillColor:pinColour,
-                fillOpacity:0.5,
-                strokeColor:pinColour,
+                fillColor: pinColour,
+                fillOpacity: 0.5,
+                strokeColor: pinColour,
                 center: window.marker.getPosition(),
                 radius: circleRadius
             });
@@ -167,3 +118,9 @@ $("#loginButton").click(function() {
     drawLightbox("loginLightbox");
     $(loginLightboxHTML).appendTo("#loginLightbox");
 });
+//Returns true if the radii of the circles overlap
+function circleOverlapTest(marker1,marker2,circle1,circle2) {
+    if (computeDistanceBetween(marker1.position,marker2.position) - circle1.radius + circle1.radius > 0) {
+        return true;
+    }
+}
